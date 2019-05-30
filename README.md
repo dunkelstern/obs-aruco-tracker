6ebf5d3f06a7902475c61bf5b8dcbc6623614572
Johannes Schriewer <hallo@dunkelstern.de>
2019-05-30 17:53:53 +0200

    Add Information about arduino sketch and STL files

--- README.md
+++ README.md
@@ -12,19 +12,19 @@ Track ArUco markers and output movement commands to re-center the marker.
 
 Add to a video source to generate camera gimbal direction output used to re-center the marker.
 
-### REQUIREMENTS
+#### REQUIREMENTS
 
 * *obs-studio*
 * *openCV 4*
 
-### INSTALLATION
+#### INSTALLATION
 
-#### For ArchLinux:
+##### For ArchLinux:
 
 Use the included PKGBUILD
 
 
-#### For installation from source:
+##### For installation from source:
 
 ```bash
 git clone https://github.com/dunkelstern/obs-aruco-tracker
@@ -37,3 +37,21 @@ make install
 ```
 
 for local installation or `cmake .. -DSYSTEM_INSTALL=1` for system installation
+
+#### Arduino sketch
+
+The Arduino sketch is used for controlling a camera gimbal (nicknamed "Klein Glotzi").
+
+You will need the Library `PWMServo` for the sketch to work.
+Before flashing the Sketch make sure that the calibration values are good for your
+Servos. (See the Sketch for more information)
+
+
+#### "Klein Glotzi" Camera gimbal
+
+![Klein Glotzi](doc/glotzi.png)
+
+- The plans for the Gimbal can be seen here: [Klein Glotzi on OnShape CAD](https://cad.onshape.com/documents/6b43bf9158c2330bf8f3274f/w/bf932df38055a7275eddfee5/e/89a3396a8a0f5c6514141639)
+- The STL Files for 3D-Printing the gimbal are in the STL Subfolder.
+- In addition to an Arduino Uno (or Nano) you will need some wires and two Tower SG90 Micro Servos.
+- The Gimbal mount is built for a MS Lifecam.
--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,36 +3,45 @@
 PWMServo servo_x;
 PWMServo servo_y;
 
+// PWM Minimum and Maximum values
 const int servo_x_pwm_min = 550;
 const int servo_x_pwm_max = 2200;
-const int servo_x_angle_min = 0;
-const int servo_x_angle_max = 180;
 
 const int servo_y_pwm_min = 450;
 const int servo_y_pwm_max = 2000;
+
+// Minimal and maximal angle of the axis without hitting any surroundings
+const int servo_x_angle_min = 0;
+const int servo_x_angle_max = 180;
+
 const int servo_y_angle_min = 70;
 const int servo_y_angle_max = 160;
 
+// Initial position (0 left, 199 right, 100 centered)
 int x = 100;
 int y = 100;
 
-
 void setup() {
+  // Using Arduino Uno we only have 2 PWM pins (9 and 10)
   servo_y.attach(SERVO_PIN_A, servo_y_pwm_min, servo_y_pwm_max);
   servo_x.attach(SERVO_PIN_B, servo_x_pwm_min, servo_x_pwm_max);
-  pinMode(13, OUTPUT);
 
+  // Initialize serial port with fast baudrate
   Serial.begin(115200);
 }
 
 void loop() {
+  // by default we do not move
   int move_x = 0;
   int move_y = 0;
 
   while (Serial.available() > 0) {
+
+    // parse first two values separated by comma
     move_x = Serial.parseInt();
     move_y = Serial.parseInt();
 
+    // and ignore the rest of the line
     while(Serial.available() > 0) {
       if (Serial.read() == '\n') {
         break;
@@ -45,6 +54,7 @@ void loop() {
 //    Serial.println(move_y);
   }
 
+  // re-attach detached servos
   if (move_y && (!servo_y.attached())) {
       servo_y.attach(SERVO_PIN_A, servo_y_pwm_min, servo_y_pwm_max);
   }
@@ -55,6 +65,7 @@ void loop() {
   x += move_x;
   y += move_y;
 
+  // clamp value range to 0 to 199
   if (x > 199) {
     x = 199;
   }
@@ -73,11 +84,15 @@ void loop() {
 //  Serial.print(x);
 //  Serial.print(", ");
 //  Serial.println(y);
-    
+  
+  // re-position servos
   servo_x.write(map(x, 0, 199, servo_x_angle_min, servo_x_angle_max));
   servo_y.write(map(y, 0, 199, servo_y_angle_min, servo_y_angle_max));
 
+  // wait 100 ms
   delay(100);
+
+  // detach servos to avoid jittering and whining
   if (servo_x.attached()) {
     servo_x.detach();
   }
